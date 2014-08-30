061d51226d23e9aca1bc4891200120d33b726e61
r-sreesaran <r.sreesaran@gmail.com>
2014-08-31 01:29:32 +0530

    Added PeerInfo
    PeerInfo a class to transmit information of peers

---
+++ src/main/java/org/javaee7/websocket/bingo/PeerInfo.java
@@ -0,0 +1,19 @@
+package org.javaee7.websocket.bingo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sree
+ */
+import org.json.simple.JSONObject;
+
+public class PeerInfo {
+    PeerInfo( ) {
+    
+    }
+}
