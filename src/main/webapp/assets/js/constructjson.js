0a55c89acf5bac75bab7076e1a6e9266e7c4efe6
r-sreesaran <r.sreesaran@gmail.com>
2014-09-18 21:41:57 +0530

    added nextplay method

--- src/main/webapp/assets/js/constructjson.js
+++ src/main/webapp/assets/js/constructjson.js
@@ -7,8 +7,17 @@
 
 var constructSinglePeerInformationJson  = function(no){
 var obj = new Object();
-obj.type="firstStart";
+obj.type="currentPlay";
 obj.id=no;
 var jsonString= JSON.stringify(obj);
 return obj;
+}
+
+var nextplay = function(currentid) {
+    for each (var session in sessionIds ) {
+      if(session.id === currentid) {
+          var currentPos=  sessionIds.indexOf(session)
+            constructSinglePeerInformationJson(currentPos == sessionIds.length-1?0:currentPos++) 
+      }
+    } 
 }
