4df3057dde3df8927c7c03c9353330a8ea00b59b
r-sreesaran <r.sreesaran@gmail.com>
2014-11-24 00:04:54 +0530

    Added Welcome Page

---
+++ src/main/webapp/assets/js/main.js
@@ -0,0 +1,18 @@
+// The coming down effect
+$.ready(function() {
+//	console.log("hi")
+//animatefunction()
+$("#welcomeText").css("opacity","1")
+});
+
+// var animateFunction() = {
+// 	$.("#welcomeText").animate({
+
+//     if(parseInt(this.css('marginLeft'))<800) {
+      
+//       'left' : "+=30px"
+//       'right':"+=30px"
+//       'opacity':"+=.25px"
+//       animatefunction
+//     }}, slow);
+//}
